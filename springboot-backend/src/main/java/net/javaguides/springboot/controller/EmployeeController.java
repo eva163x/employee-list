ae4915e58d3fded3cdfcfa1228cb5cd89ee45934
Eva Aggarwal <eva.aggarwal@duke.edu>
2024-08-06 12:46:39 +0530

    createEmployee api

--- springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package net.javaguides.springboot.controller;
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +18,10 @@ public class EmployeeController {
     @GetMapping
     public List<Employee> getAllEmployees() { //just returns list of all employees to client
         return employeeRepository.findAll();
+    }
+
+    @PostMapping()
+    public Employee createEmployee(@RequestBody Employee employee){ //saves new employee to repository & database
+        return employeeRepository.save(employee);
     }
 }
